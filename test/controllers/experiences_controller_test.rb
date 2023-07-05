d0f005dff1a1166b7fab0cb16fa2c80936e78dc2
Ricardo Delgado <rickydel19@gmail.com>
2023-07-05 12:44:47 -0500

    Created create action

--- test/controllers/experiences_controller_test.rb
+++ test/controllers/experiences_controller_test.rb
@@ -12,4 +12,11 @@ class ExperiencesControllerTest < ActionDispatch::IntegrationTest
     data = JSON.parse(response.body)
     assert_equal Experience.count, data.length
   end
+
+  test "create" do
+    assert_difference "Experience.count", 1 do
+      post "/experiences.json", params: { start_date: "date", end_date: "date", job_title: "test", company_name: "test", details: "Test" }
+      assert_response 200
+    end
+  end
 end
