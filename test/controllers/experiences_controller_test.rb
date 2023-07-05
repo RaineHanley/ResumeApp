c35fdef4eb1c0772616bb407fc315eed69f2ddd3
Ricardo Delgado <rickydel19@gmail.com>
2023-07-05 12:40:21 -0500

    Added index action

--- test/controllers/experiences_controller_test.rb
+++ test/controllers/experiences_controller_test.rb
@@ -4,4 +4,12 @@ class ExperiencesControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+
+  test "index" do
+    get "/experiences.json"
+    assert_response 200
+
+    data = JSON.parse(response.body)
+    assert_equal Experience.count, data.length
+  end
 end
