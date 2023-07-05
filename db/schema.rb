93980fac01fe66d4b25a9d1360e0e6dd0b260d61
Ricardo Delgado <rickydel19@gmail.com>
2023-07-05 15:48:13 -0500

    Added cors file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_05_171903) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_05_181507) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -31,4 +31,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_05_171903) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
 end
