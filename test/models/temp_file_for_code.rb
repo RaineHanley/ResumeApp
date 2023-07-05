0ddc148785a05bf762ec6f679b0bda143eb25989
Shawn Goff <shawngoff@Shawns-MacBook-Pro.local>
2023-07-05 10:37:26 -0700

    Add model with seeds for initial data

--- test/models/temp_file_for_code.rb
+++ test/models/temp_file_for_code.rb
@@ -1,10 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+First Name
+Last Name
+Email
+Phone Number
+Short Bio
+LinkedIn URL
+Twitter Handle
+Personal Blog
+Online Resume URL
+Github URL
+Photo
+
 Student.create(first_name: "John", last_name: "Johnson", email: "johnjohnson@test.com", phone_number: "555-555-5555", short_bio: "This is my short bio", linkedin_url: "https://linkedin.com/johnjohnson", twitter_handle: "@JohnJohnson", personal_website_url: "www.johnjohnson.com", online_resume_url: "www.johnjohnsonsresume.com", github_url: "www.github.com/johnjohnson", photo: "image@test.com")
 
 Student.create(first_name: "Joe", last_name: "Smith", email: "joesmith@test.com", phone_number: "444-444-4444", short_bio: "This is my short bio", linkedin_url: "https://linkedin.com/joesmith", twitter_handle: "@JoeSmith", personal_website_url: "www.joesmith.com", online_resume_url: "www.joesmithssresume.com", github_url: "www.github.com/joesmith", photo: "image@test.com")
