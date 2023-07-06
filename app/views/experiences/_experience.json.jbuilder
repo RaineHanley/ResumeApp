027fb3e6c863d9509a8167d61b69db339c0d61ec
Ricardo Delgado <rickydel19@gmail.com>
2023-07-06 14:18:00 -0500

    Add relationship to experiences table for student_id

--- app/views/experiences/_experience.json.jbuilder
+++ app/views/experiences/_experience.json.jbuilder
@@ -4,3 +4,4 @@ json.end_date experience.end_date
 json.job_title experience.job_title
 json.company_name experience.company_name
 json.details experience.details
+json.student_id experience.student_id
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_06_164623) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_06_185442) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -41,6 +41,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_06_164623) do
     t.text "details"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "student_id"
   end
 
   create_table "skills", force: :cascade do |t|
@@ -72,4 +73,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_06_164623) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
 end
