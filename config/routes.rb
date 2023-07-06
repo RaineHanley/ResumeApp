ae54ce2e51b7b50c025595a598fe3e4236e1aebb
Shawn Goff <shawngoff@Shawns-MacBook-Pro.local>
2023-07-06 10:30:56 -0700

    Add model with seeds for initial data

--- config/routes.rb
+++ config/routes.rb
@@ -13,16 +13,19 @@ Rails.application.routes.draw do
   post "/users" => "users#create"
 
   post "/sessions" => "sessions#create"
-  
+
   get "/skills" => "skills#index"
   post "/skills" => "skills#create"
   get "/skills/:id" => "skills#show"
   patch "/skills/:id" => "skills#update"
   delete "/skills/:id" => "skills#destroy"
-  
+
   get "/students" => "students#index"
   get "/students/:id" => "students#show"
   post "/students" => "students#create"
   patch "/students/:id" => "students#update"
   delete "/students/:id" => "students#destroy"
 end
+
+Education.create(start_date: "08/12/15", end_date: "05/27/19", degree: "Bachelor's of Science", university_name: "San Francisco State University", details: "Here are the details")
+Education.create(start_date: "08/12/15", end_date: "05/27/19", degree: "Bachelor's of Arts", university_name: "San Jose State University", details: "Here are the details")
--- db/schema.rb
+++ db/schema.rb
@@ -10,22 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_05_172051) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_06_172641) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "students", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.string "email"
-    t.string "phone_number"
-    t.text "short_bio"
-    t.string "linkedin_url"
-    t.string "twitter_handle"
-    t.string "personal_website_url"
-    t.string "online_resume_url"
-    t.string "github_url"
-    t.string "photo"
+  create_table "educations", force: :cascade do |t|
+    t.string "start_date"
+    t.string "end_date"
+    t.string "degree"
+    t.string "university_name"
+    t.text "details"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "experiences", force: :cascade do |t|
@@ -44,6 +40,22 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_05_172051) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "students", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "phone_number"
+    t.text "short_bio"
+    t.string "linkedin_url"
+    t.string "twitter_handle"
+    t.string "personal_website_url"
+    t.string "online_resume_url"
+    t.string "github_url"
+    t.string "photo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -51,4 +63,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_05_172051) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
 end
