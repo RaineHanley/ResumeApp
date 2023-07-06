7a1e55182657d1af2baba3824bf7e659196dd656
Shawn Goff <shawngoff@Shawns-MacBook-Pro.local>
2023-07-06 12:21:05 -0700

    Added education relationships

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_06_164623) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_06_172641) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -72,4 +72,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_06_164623) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
 end
