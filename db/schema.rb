63fa42d292c4d5f9bd705adc8f2708dd6f9c28ce
Ricardo Delgado <rickydel19@gmail.com>
2023-07-07 11:48:38 -0500

    Added education seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_06_190536) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_06_191142) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -75,4 +75,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_06_190536) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
 end
